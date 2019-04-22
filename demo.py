de5c2322b8d2abaf981022dee4248f78fd284af6
K.R. Zentner <krzentner@gmail.com>
2019-04-22 14:41:30 -0700

    WIP demo

---
+++ demo.py
@@ -0,0 +1,44 @@
+import os
+import template_matcher
+
+net = template_matcher.TemplateMatcher()
+
+try:
+    checkpoint = torch.load('template_matcher_askubuntu_latest.pt')
+    net.load_state_dict(checkpoint['model'])
+    torch.save(net, f'template_matcher_askubuntu_latest.model')
+    epoch = checkpoint['epoch']
+    print(f'Successfully loaded epoch {epoch}.')
+except Exception as e:
+    print('Could not load template matcher.')
+    raise e
+
+templates = [
+    ['Go', 'to', '<dir>'],
+    ['Make', 'a', 'directory', 'called', '<dir>'],
+    ['List', '<dir>'],
+    ]
+
+encoded_templates = net.encode_templates(templates)
+
+while True:
+    print('engsh: ', end='', flush=True)
+    user_input = input().strip().split(' ')
+    slot_matches, template_matches = net(encoded_templates, [user_input])
+    which_template = template_matches[0].argmax()
+    match_likelihood = template_matches[0][which_template]
+    matched_template = templates[which_template]
+    print(f'I think you said: {matched_template!r} with likelihood {match_likelihood!r}')
+    slot_idx = slot_matches[0][0][0].argmax()
+    if which_template == 0:
+        dirname = user_input[slot_idx]
+        os.chdir(dirname)
+    elif which_template == 1:
+        dirname = user_input[slot_idx]
+        os.mkdir(dirname)
+    else:
+        dirname = user_input[slot_idx]
+        if 'current' in dirname:
+            os.listdir()
+        else:
+            os.listdir(dirname)
